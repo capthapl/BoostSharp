28720b85f0bb4b6de3a90f18f2c8714addfdff2a
Dawid Kacprzak <dawidkacprzak@icloud.com>
2019-06-24 17:02:53 +0200

    Init

---
+++ BoostSharp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Auth;
+using Microsoft.AppCenter;
+
+namespace BoostSharp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("ios=feb09ce2-4c16-4987-b14c-5de5a9176d81;",typeof(Analytics), typeof(Crashes), typeof(Auth));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
