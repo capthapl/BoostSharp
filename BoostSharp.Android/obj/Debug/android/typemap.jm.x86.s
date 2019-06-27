98536dd2054e5285e00431e097f5174237530d54
capthapl <dawidkacprzak@icloud.com>
2019-06-27 21:40:01 +0200

    Updated libs and added app icon for iOS

---
+++ BoostSharp.Android/obj/Debug/android/typemap.jm.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 1b25c5b801fc869cfd58cabb6a627eca729a57a7 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1693
+	/* entry-length */
+	.long	280
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 474041
+	.include	"typemap.jm.inc"
