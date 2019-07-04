50fdea3b6bd31e302215b33ca3f2b58eba8c25aa
Dawid Kacprzak <dawidkacprzak@icloud.com>
2019-07-04 20:58:38 +0200

    Added Chip custom control

---
+++ BoostSharp/CustomControls/Chip.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using BoostSharp.CustomControls.ViewModels;
+using Xamarin.Forms;
+
+namespace BoostSharp.CustomControls
+{
+    public partial class Chip : ContentView
+    {
+        public ChipViewModel ChipViewModel { get; set; }
+
+        public ICommand OnClick {
+            get{
+                return ChipViewModel.TapContainer;
+            }
+            set
+            {
+                ChipViewModel.TapContainer = value;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return ChipViewModel.Text;
+            }
+            set
+            {
+                ChipViewModel.Text = value;
+            }
+        }
+
+        public Chip()
+        {
+            ChipViewModel = new ChipViewModel();
+            InitializeComponent();
+            BindingContext = ChipViewModel;
+        }
+    }
+}
