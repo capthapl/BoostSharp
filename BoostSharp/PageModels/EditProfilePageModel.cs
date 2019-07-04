ca1654c94511fad17b72af2d80ab1b1338c4fe5b
Jakub Fladzinski <fladzio321@gmail.com>
2019-07-05 00:07:38 +0200

    Created EditProfilePage

---
+++ BoostSharp/PageModels/EditProfilePageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreshMvvm;
+
+namespace BoostSharp.PageModels
+{
+    class EditProfilePageModel : FreshBasePageModel
+    {
+    }
+}
