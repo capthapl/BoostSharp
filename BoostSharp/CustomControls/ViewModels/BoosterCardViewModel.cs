3efb13e186b0cc3402e5a3450b16d522e7156ae3
capthapl <dawidkacprzak@icloud.com>
2019-07-04 23:20:58 +0200

    Created booster card with sample data

---
+++ BoostSharp/CustomControls/ViewModels/BoosterCardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BoostSharp.CustomControls.ViewModels
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class BoosterCardViewModel 
+    {
+        public Uri ImageSource { get; set; }
+    }
+}
+
