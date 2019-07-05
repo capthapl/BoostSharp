3f5e35b270e40eac597677a3e06895a468727dbf
Jakub Fladzinski <fladzio321@gmail.com>
2019-07-05 23:54:00 +0200

    Filter Edit

--- BoostSharp/Pages/HomePage.xaml.cs
+++ BoostSharp/Pages/HomePage.xaml.cs
@@ -11,5 +11,11 @@ namespace BoostSharp.Pages
         {
             InitializeComponent();
         }
+
+        void OpenCloseFilter(object sender, EventArgs e)
+        {
+            _filter.IsVisible = !_filter.IsVisible;
+            _boosters.IsVisible = !_boosters.IsVisible;
+        }
     }
 }
