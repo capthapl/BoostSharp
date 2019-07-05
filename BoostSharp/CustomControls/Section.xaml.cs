fff573d8b16b4bce22f6d3ed59bc1a9fa969d565
capthapl <john@doe.org>
2019-07-05 22:47:41 +0200

    Created section component

---
+++ BoostSharp/CustomControls/Section.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using BoostSharp.CustomControls.ViewModels;
+using Xamarin.Forms;
+
+namespace BoostSharp.CustomControls
+{
+    [ContentProperty("VisibleChildren")]
+    public partial class Section : ContentView
+    {
+        public StackLayout VisibleLayout { get; set; }
+
+        public IList<View> VisibleChildren => VisibleLayout.Children;
+
+        public SectionViewModel SectionViewModel { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return SectionViewModel.Title;
+            }
+            set
+            {
+                SectionViewModel.Title = value;
+            }
+        }
+
+        public Section()
+        {
+            InitializeComponent();
+            SectionViewModel = new SectionViewModel();
+            VisibleLayout = content;
+            BindingContext = SectionViewModel;
+        }
+    }
+}
---
+++ BoostSharp/CustomControls/ViewModels/SectionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Xamarin.Forms;
+namespace BoostSharp.CustomControls.ViewModels
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class SectionViewModel
+    {
+        public string Title { get; set; }
+    }
+}
+
