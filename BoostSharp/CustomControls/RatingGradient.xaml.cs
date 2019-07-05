c0b3b29d7511c1477f3972641c24fd35b76def7d
Dawid Kacprzak <dawidkacprzak@icloud.com>
2019-07-05 10:20:27 +0200

    Created rating gradient control
     Author:    capthapl <dawidkacprzak@icloud.com>
     Date:      Fri Jul 5 10:20:27 2019 +0200

---
+++ BoostSharp/CustomControls/RatingGradient.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using BoostSharp.CustomControls.ViewModels;
+using Xamarin.Forms;
+
+namespace BoostSharp.CustomControls
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public partial class RatingGradient : ContentView
+    {
+        RatingGradientViewModel RatingGradientViewModel { get; set; }
+
+        private double _ratingInPercent;
+        public double RatingInPercent
+        {
+            get
+            {
+                return _ratingInPercent;
+            }
+            set
+            {
+                _ratingInPercent = value;
+                InvalidateLayout();
+            }
+        }
+    
+        public RatingGradient()
+        {
+            InitializeComponent();
+            RatingGradientViewModel = new RatingGradientViewModel();
+            BindingContext = RatingGradientViewModel;
+        }
+
+        protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+        {
+            double marginLeft = ((widthConstraint / 100) * RatingInPercent)-5;
+            if (marginLeft < 0) marginLeft = 0;
+            RatingGradientViewModel.IndicatorMarginPosition = new Thickness(marginLeft, 0, 0, 0);
+
+            return base.OnMeasure(widthConstraint, heightConstraint);
+        }
+    }
+}
---
+++ BoostSharp/CustomControls/ViewModels/RatingGradientViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BoostSharp.CustomControls.ViewModels
+{
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class RatingGradientViewModel
+    {
+        public Thickness IndicatorMarginPosition { get; set; }
+    }
+}
