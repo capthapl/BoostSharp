bb8bfc450440ae7250a82b10314e2d0fec76be37
capthapl <john@doe.org>
2019-07-06 00:03:54 +0200

    Binded navigation arrow to command

--- BoostSharp/PageModels/ProfilePageModel.cs
+++ BoostSharp/PageModels/ProfilePageModel.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Windows.Input;
 using FreshMvvm;
+using Xamarin.Forms;
 
 namespace BoostSharp.PageModels
 {
     class ProfilePageModel : FreshBasePageModel
     {
+        public ICommand PopCommand { get; set; }
+
+        public ProfilePageModel()
+        {
+            PopCommand = new Command(async () =>
+            {
+                await CoreMethods.PopPageModel(this);
+            });
+        }
     }
 }
