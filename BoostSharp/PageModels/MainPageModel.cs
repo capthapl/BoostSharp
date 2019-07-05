6170e16877b36a6e3684172557ffd2b4385c6bfe
capthapl <john@doe.org>
2019-07-05 17:06:46 +0200

    Added open term or rules feature on MainPage

--- BoostSharp/PageModels/MainPageModel.cs
+++ BoostSharp/PageModels/MainPageModel.cs
@@ -8,12 +8,19 @@ namespace BoostSharp.PageModels
     public class MainPageModel : FreshBasePageModel
     {
         public ICommand OpenHomePageCommand { get; set; }
+        public ICommand OpenTermsCommand { get; set; }
         public override void Init(object initData)
         {
             base.Init(initData);
             OpenHomePageCommand = new Command(() =>
             {
                 CoreMethods.PushPageModel<HomePageModel>();
+            });
+            TapGestureRecognizer t = new TapGestureRecognizer();
+
+            OpenTermsCommand = new Command(() =>
+            {
+                Device.OpenUri(new Uri(@"https://firebasestorage.googleapis.com/v0/b/poc-files.appspot.com/o/Polityka%20BoostSharp.pdf?alt=media&token=61c70183-6cf3-4802-84b7-13946070a660"));
             });
         }
     }
